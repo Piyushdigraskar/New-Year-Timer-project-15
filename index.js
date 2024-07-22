b4aea6b7853aca62b0e001db42b4c85b45172d25
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-07-22 22:53:34 +0530

    Added Current  Year

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const dayEl = document.getElementById("day");
 const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minute");
 const secondEl = document.getElementById("second");
+const yearEl = document.getElementById("year");
 
 
 const newYearTime = new Date("jan 1, 2025 00:00:00").getTime();
+
 
 updateCountDown();
 function updateCountDown(){
@@ -20,11 +22,14 @@ function updateCountDown(){
     const h = Math.floor((gap%day)/ hour)
     const m = Math.floor((gap%hour)/ minute)
     const s = Math.floor((gap%minute)/ second)
+    const y = new Date().getFullYear();
 
     dayEl.innerText = d;
     hourEl.innerText = h;
     minuteEl.innerText = m;
     secondEl.innerText = s;
+    console.log(y);
+    yearEl.innerText = y;
     setTimeout(updateCountDown, 1000);
 }
 
